2b18654c23ba66ebbe64fd3a3069c19a075832e9
dorjoo <dorjoohover@gmail.com>
2025-08-17 15:45:14 +0800

    a

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,8 +9,8 @@ const BASE = process.env.API
   ? process.env.API?.endsWith("/")
     ? process.env.API
     : process.env.API + "/"
-  : "http://localhost:4000/api/v1/";
-// :  "https://srv952218.hstgr.cloud/api/v1/";
+  // : "http://localhost:4000/api/v1/";
+:  "https://srv952218.hstgr.cloud/api/v1/";
 
 export enum Api {
   login = "login",
@@ -65,4 +65,4 @@ export const API = {
 };
 
 export const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://zuunailbar-admin-jzsw.vercel.app/";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://zuunailbar-web.vercel.app";
